b14173c251a979e234b6e21b840107acc429d2d8
thankuzzz <96867490+thankuzzz@users.noreply.github.com>
2022-09-06 11:44:19 +0530

    Add files via upload

---
+++ basic assignments/Q9.dart
@@ -0,0 +1,21 @@
+//Write a program to generate Prime numbers between 1 and 100
+
+import 'dart:io';
+
+void main() {
+  var a;
+  print("Prime numberts");
+  for (int i = 1; i <= 100; i++) {
+    a = 0;
+    for (int j = 1; j <= 100; j++) {
+      if (i % j == 0)
+        a++;
+    }
+    if (a == 2) {
+      stdout.write("$i, ");
+    }
+  }
+}
+
+
+
